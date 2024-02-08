25bbd54d9a96ace1861dcd9be36b8cf9dee0e6a4
adelina <adelinafigueiraf@gmail.com>
2024-02-08 00:14:20 -0400

    bfs

--- src/project/BFSAlgorithm.cpp
+++ src/project/BFSAlgorithm.cpp
@@ -5,10 +5,65 @@
 #include <assert.h>
 #include <sys/time.h>
 #include <queue>
+#include <chrono>
 
 #include "BFS.hpp"
+#define MAX_RUN_TIME 420000
 
-std::optional<int> BFS(state_t root){
+using namespace std::chrono;
+
+
+int64_t runtime(system_clock::time_point &from) {
+    int64_t currentTime = duration_cast<milliseconds>(high_resolution_clock::now() - from).count();
+    return currentTime > MAX_RUN_TIME ? currentTime : -1;
+}
+
+std::optional<int> BFS(state_t state){
+	printf("entra a bfs");
+	int64_t auxTime;
+    system_clock::time_point start = high_resolution_clock::now();
+
+
+	std::queue<state_t> q;
+    ruleid_iterator_t iter;
+    q.emplace(state);
+
+
+    int currentLevel = 1;
+    int nextLevel = 1;
+    int size = 0;
+    int rule;
+
+    while (!q.empty()){
+        // printf("Aun no esta vacia, nodo %d\n",size);
+        
+        state_t currentVertex = q.front(); q.pop();
+        init_fwd_iter(&iter, &currentVertex);
+
+        if (currentLevel == 0) 
+            {
+            	size +=1;
+                currentLevel = nextLevel;
+                nextLevel = 0;
+                printf("Profundidad : %d \n",size);
+                printf("Nodos : %d \n",currentLevel);
+            }
+
+        if (is_goal(&currentVertex)){
+            printf("Llego al final %d \n",size);
+            printf("Tiempo: %ld",duration_cast<milliseconds>(high_resolution_clock::now() - start).count());
+        }
+        while ((rule = next_ruleid(&iter)) >= 0) {
+                state_t s;
+                apply_fwd_rule(rule, &currentVertex, &s);
+
+                q.emplace(s);
+                nextLevel += 1;
+        }
+        printf("This node has %d children\n",size);
+        currentLevel--;
+
+    }
 	return 0;
 }
 
--- src/project/bfs.cpp
+++ src/project/bfs.cpp
@@ -15,44 +15,5 @@ int main(int argc, char **argv){
     BFS(state);
     BFS_Prunning(state);
 
-
-    std::queue<state_t> q;
-    ruleid_iterator_t iter;
-    q.emplace(state);
-
-
-    int currentLevel = 1;
-    int nextLevel = 1;
-    int size = 0;
-    int action;
-
-    while (!q.empty()){
-        printf("Aun no esta vacia, nodo %d\n",size);
-        
-        state_t currentVertex = q.front(); q.pop();
-        init_fwd_iter(&iter, &currentVertex);
-
-        if (currentLevel == 0) 
-            {
-                currentLevel = nextLevel;
-                nextLevel = 0;
-                printf("Profundidad : %d \n",size);
-            }
-
-        if (is_goal(&currentVertex)){
-            printf("Llego al final %d \n",size);
-        }
-        while ((action = next_ruleid(&iter)) >= 0) {
-                state_t s;
-                apply_fwd_rule(action, &currentVertex, &s);
-
-                q.emplace(s);
-                nextLevel += 1;
-                size +=1;
-        }
-        currentLevel--;
-
-    }
-
 	return 0;
 }
